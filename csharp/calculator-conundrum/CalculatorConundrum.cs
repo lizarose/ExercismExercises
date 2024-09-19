a0605e2e61ac4647630fa0fa2fb4ffc259205b73
Elizabeth Levinson <elizabeth.levinson@qbcinc.com>
2024-09-19 09:43:28 -0500

    Updated Switch to have Null and Empty, and fixed DivideByZeroException

--- csharp/calculator-conundrum/CalculatorConundrum.cs
+++ csharp/calculator-conundrum/CalculatorConundrum.cs
@@ -1,35 +1,29 @@
 using System;
-
 public static class SimpleCalculator
 {
     public static string Calculate(int operand1, int operand2, string operation)
     {
          try
-        {
-             if (operation == null)
-            {
-                 throw new ArgumentNullException("Null Operation");
-            }
-            else if (operation == "")
-            {
-                  throw new ArgumentException("No Value Entered for Operation");
-            }
-            
+        {            
             switch (operation)
             {
+                case null:
+                    throw new ArgumentNullException("Null Operation");
+                case "":
+                    throw new ArgumentException("No Value Entered for Operation");
                 case "+": 
                     return $"{operand1} + {operand2} = {operand1 + operand2}";
                 case "*":
                     return $"{operand1} * {operand2} = {operand1 * operand2}";
                 case "/":
-                    return operand2 != 0 ? $"{operand1} / {operand2} = {operand1 / operand2}": "Division by zero is not allowed.";
+                    return $"{operand1} / {operand2} = {operand1 / operand2}";
                 default:
                     throw new ArgumentOutOfRangeException("Non Valid Operation");
             }
         }
-        catch (ArgumentOutOfRangeException)
+        catch (DivideByZeroException)
         {
-            throw new ArgumentOutOfRangeException("Non-Valid Operation");
+            return "Division by zero is not allowed.";
         }
     
     }
