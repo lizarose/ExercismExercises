dc4b00b13f58c57373422b2cc2e2d89abe137c23
Elizabeth Levinson <elizabeth.levinson@qbcinc.com>
2024-09-24 16:21:33 -0500

    Read Correctness and took notes

---
+++ fsharp/Correctness.fsx
@@ -0,0 +1,10 @@
+(* Correctness - How to Write Compile Time Unit Tests*)
+
+(*
+    -should be able to look at code and easily read and understand it
+    F# Features to Ensure Correctness:
+        -immutability --> code behaves more predictable
+        -exhaustive pattern matching --> traps common errors at compile
+        -strict type system --> check instant compile unit tests
+        -expressive type system --> make illegal states unprepresentable
+*)
