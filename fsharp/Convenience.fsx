7d906f1d2288bb5b51f83e02bc02d0e7ee5e7ec9
Elizabeth Levinson <elizabeth.levinson@qbcinc.com>
2024-09-24 15:16:48 -0500

    Read Convenience and took notes

---
+++ fsharp/Convenience.fsx
@@ -0,0 +1,10 @@
+(*Convenience - Reduce Drudgery and Boilerplate Code*)
+
+(*
+    -useful "out-of-the-box" behavior for types
+    -all functions are "interfaces"
+        -many of the roles that interfaces play in object-oriented design
+            patterns are unnecessary within a funcitonal paradigm
+    -partial application
+    -active patterns
+*)
