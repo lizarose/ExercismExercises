0ad5fcabe3de13596e6834a4ccdf4ccf8c1dec29
Elizabeth Levinson <elizabeth.levinson@qbcinc.com>
2024-09-25 10:53:03 -0500

    Updated with notes on lists and pattern matching

--- fsharp/BasicsF.fsx
+++ fsharp/BasicsF.fsx
@@ -150,3 +150,79 @@ match number with
     associated with that pattern
         the order of the patterns matters!
 *)
+
+
+
+
+
+
+
+
+
+
+
+(* Lists - Tracks On Tracks On Tracks exercise*)
+
+(*
+    -a list is an immutable collection of zero or more values
+    -the values in a list must all have the same type
+    -once a list has been constructed, its value can never change
+    -any functions/operators that appear to modify a list (such as adding
+        an element), will actually return a new list
+*)
+
+let empty = []
+let singleValue = [5]
+let threeValues = ["a"; "b"; "c"]
+
+//the most common way to add an element to a list is through the :: (cons) operator
+
+let twoToFour = [2; 3; 4]
+let oneToFour = 1 :: twoToFour
+    //result --> [1; 2; 3; 4]
+
+
+
+(*
+    -a list has a head (the first element) and a tail (everythig after the first element)
+    -the tail of a list is itself a list
+
+    -lists are either manipulated by functions and operators defined in the List module, or manually using pattern matching using the list and cons patterns:
+*)
+
+let describe list =
+    match list with 
+    | [] -> "Empty list"
+    | head :: tail -> sprintf "Non-empty list with head: %d" head
+
+describe []
+    //result --> "Empty list"
+describe [1]
+    //result --> "Non-empty list with head: 1"
+describe [5; 7; 9]
+    //result --> "Non-empty list with head: 5"
+
+
+(*
+    -you can also discard a value when pattern matching
+    -when you do not care about a value in a specific case or just won't use that value, you use and underscore _ 
+
+    _ should always come last when pattern matching, as every value that 
+    doesn't match any of teh other cases will be handled by this case
+*)
+
+let describe list =
+    match list with
+    | [] -> "Empty list"
+    | [x] -> "List with one item"
+    | [_; y] -> "List with two items (first item ignored)"
+    | _ -> "List with many items (all items ignored)"
+
+describe []
+    //result --> "Empty list"
+describe [1]       
+    //result --> "List with one item"
+describe [5; 7]     
+    //result --> "List with two items (first item ignored)"
+describe [5; 7; 9] 
+    //result --> "List with many items (all items ignored)"
