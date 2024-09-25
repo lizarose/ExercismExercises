699bd549c9cbce7a0b91e44ef78f0742bfea3b9e
Elizabeth Levinson <elizabeth.levinson@qbcinc.com>
2024-09-25 09:59:27 -0500

    Completed and Passed Defining Functions

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,37 @@
+module LuciansLusciousLasagna
+
+// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
+
+// TODO: define the 'remainingMinutesInOven' function
+let currentOvenTimeIn = 30
+let remainingMinutesInOven currentOvenTimeIn = expectedMinutesInOven - currentOvenTimeIn
+
+// TODO: define the 'preparationTimeInMinutes' function
+let layers = 2
+let preparationTimeInMinutes layers = layers * 2
+
+// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes layers currentOvenTimeIn = preparationTimeInMinutes layers + currentOvenTimeIn
+
+
+(*
+    - You have layers and currentOvenTimeIn as parameters going in to elapsedTimeInMinutes.
+    - To use preparationTimeInMinutes you need to have layers with it bc it takes layers in as an argument.
+
+    - example in C#:
+
+
+public int preparationTimeInMinutes(int layers) {
+    return layers * 2;
+}
+public int elapsedTimeInMinutes(int layers, int currentOvenTimeIn) {
+    return preparationTimeInMinutes(layers) + currentOvenTimeIn;
+}
+
+
+*)
+
+
+
+
