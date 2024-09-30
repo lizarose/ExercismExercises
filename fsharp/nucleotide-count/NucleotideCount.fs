b945a2b95a7861e33ad8d34f36a223bf7088f0bc
lizarose <elizabeth.levinson@ekonbenefits.com>
2024-09-30 08:56:40 -0500

    Added notes for lists and maps

---
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,19 @@
+module NucleotideCount
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> =  failwith "You need to implement this function."
+
+
+
+
+
+
+
+
+(*
+    List.ofSeq --> takes a sequence and turns to a list
+    List.forall --> looks to see if a functions is true for all elements being tested
+    Map.ofList --> converts a list to a map
+
+    map --> similar to a dictionary in C#
+    map.containsKey --> looks for key
+*)
