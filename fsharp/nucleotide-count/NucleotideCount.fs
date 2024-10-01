42463490782081c6da8f15582464592faffecfce
lizarose <elizabeth.levinson@ekonbenefits.com>
2024-10-01 10:31:48 -0500

    Previous version was tail recursion. This fixes the stackoverflow problem by using Seq.fold

--- fsharp/nucleotide-count/NucleotideCount.fs
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -3,17 +3,21 @@ module NucleotideCount
 let nucleotideCounts (strand: string): Option<Map<char, int>> =  
     //initial map creation
     let initialCharCount = Map.ofList [('A', 0); ('C', 0); ('G', 0); ('T', 0)]
-    //recursive function that takes in a counter and the chars that still need to be iterated through
-    let rec countNucleotides nucleotideMap remainingChars = 
-        match remainingChars with 
-        | [] -> Some nucleotideMap      //base case: returns final count of valid nucleotides
-        | char :: remainingChars ->     //pattern matches chars 
-            match Map.tryFind char nucleotideMap with 
-            | Some currentCount -> countNucleotides (Map.add char (currentCount + 1) nucleotideMap) remainingChars  //this is valid chars so count is updated
-            | None -> None              //invalid nucleotide
+    //count nucleotides and check if valid nucleotides
+    let countNucleotides nucleotideMap char = 
+        match Map.tryFind char nucleotideMap with 
+        | Some currentCount -> Map.add char (currentCount + 1) nucleotideMap
+        | None -> Map.empty     //if not valid nucleotides, then return empty map
 
-    strand |> Seq.toList |> countNucleotides initialCharCount       //converts strand to list then calls function with initial map to start recursion
-  
+    //use Seq.fold to calculate the nucleotide count
+    let result = strand |> Seq.fold countNucleotides initialCharCount
+
+    //check to see if empty to know if valid nucleotides or not
+    if Map.isEmpty result then    
+        None
+    else 
+        Some result
+
 
 
 
@@ -31,5 +35,32 @@ let nucleotideCounts (strand: string): Option<Map<char, int>> =
     As the 'yield' keyword pushes a single value into a list, the keyword, 'yield!', pushes a collection of values into the list.
 
 
-    this is basically tail recursion but problem is it doesn't handle long strands so it won't stackoverflow
+
+
+
+
+
+
+
+
+
+    this is basically tail recursion but problem is it doesn't handle long strands so it won't stackoverflow:
+module NucleotideCount
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> =  
+    //initial map creation
+    let initialCharCount = Map.ofList [('A', 0); ('C', 0); ('G', 0); ('T', 0)]
+    //recursive function that takes in a counter and the chars that still need to be iterated through
+    let rec countNucleotides nucleotideMap remainingChars = 
+        match remainingChars with 
+        | [] -> Some nucleotideMap      //base case: returns final count of valid nucleotides
+        | char :: remainingChars ->     //pattern matches chars 
+            match Map.tryFind char nucleotideMap with 
+            | Some currentCount -> countNucleotides (Map.add char (currentCount + 1) nucleotideMap) remainingChars  //this is valid chars so count is updated
+            | None -> None              //invalid nucleotide
+
+    strand |> Seq.toList |> countNucleotides initialCharCount       //converts strand to list then calls function with initial map to start recursion
+  
+
+
 *)
