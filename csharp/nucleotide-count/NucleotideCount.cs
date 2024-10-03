fe0cca3fc6c202e61a1a2a87fe55ce630af8f5d7
lizarose <elizabeth.levinson@ekonbenefits.com>
2024-10-03 08:09:58 -0500

    Updated with functional C# using LINQ

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,30 +1,49 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
-    {
-        Dictionary<char, int> charCount =
-            new Dictionary<char, int>();
-
-        charCount.Add('A', 0);
-        charCount.Add('C', 0);
-        charCount.Add('G', 0);
-        charCount.Add('T', 0);
-
-
-        foreach(char character in sequence)
-        {
-           if(charCount.ContainsKey(character))
-           {
-                charCount[character]++;
-           }
-            else
-            {
-                throw new ArgumentException("Invalid Sequence");
-            }
-        }
-        return charCount;
+    {     
+        var validNucleotides = new HashSet<char> {'A', 'C', 'G', 'T'};   
+        var nucleotidesCount = validNucleotides.ToDictionary(n => n, n => 0);
+        
+        if (sequence.Any(n => !validNucleotides.Contains(n))) 
+            throw new ArgumentException("Invalid Sequence");
+        
+        sequence.ToList().ForEach(n => nucleotidesCount[n]++);
+        return nucleotidesCount;
     }
 }
+
+
+
+
+// public static class NucleotideCount
+// {
+//     public static IDictionary<char, int> Count(string sequence)
+//     {
+//         Dictionary<char, int> charCount =
+//             new Dictionary<char, int>();
+
+//         charCount.Add('A', 0);
+//         charCount.Add('C', 0);
+//         charCount.Add('G', 0);
+//         charCount.Add('T', 0);
+
+
+//         foreach(char character in sequence)
+//         {
+//            if(charCount.ContainsKey(character))
+//            {
+//                 charCount[character]++;
+//            }
+//             else
+//             {
+//                 throw new ArgumentException("Invalid Sequence");
+//             }
+//         }
+//         return charCount;
+//     }
+// }
