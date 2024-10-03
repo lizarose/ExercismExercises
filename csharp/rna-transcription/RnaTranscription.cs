21cd75dcd66186e7afe71044b8ef182cb612977b
lizarose <elizabeth.levinson@ekonbenefits.com>
2024-10-03 08:56:20 -0500

    Updated with functional C# using LINQ

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -1,31 +1,44 @@
 using System;
+using System.Linq;
 
 public static class RnaTranscription
 {
-    public static string ToRna(string strand)
-    {
-        string rna = "";
-
-        foreach(char character in strand)
-        {
-            switch (character)
-            {
-               case 'A':
-                   rna += 'U';
-                   break;
-               case 'C':
-                   rna += 'G';
-                   break;
-               case 'G':
-                   rna += 'C';
-                   break;
-               case 'T':
-                   rna += 'A';
-                   break;
-               default:
-                   throw new ArgumentException("Invalid Sequence");
-            }
-        }
-        return rna;
-    }
+     public static string ToRna(string strand) => string.Join("", strand.Select(n => n switch
+        { 'A' => 'U', 
+          'C' => 'G', 
+          'G' => 'C', 
+          'T' => 'A' }
+    ));
 }
+
+
+
+// public static class RnaTranscription
+// {
+//     public static string ToRna(string strand)
+//     {
+//         string rna = "";
+
+//         foreach(char character in strand)
+//         {
+//             switch (character)
+//             {
+//                case 'A':
+//                    rna += 'U';
+//                    break;
+//                case 'C':
+//                    rna += 'G';
+//                    break;
+//                case 'G':
+//                    rna += 'C';
+//                    break;
+//                case 'T':
+//                    rna += 'A';
+//                    break;
+//                default:
+//                    throw new ArgumentException("Invalid Sequence");
+//             }
+//         }
+//         return rna;
+//     }
+// }
